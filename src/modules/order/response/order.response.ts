c13df3e2c9cb19b92f430e5c8a327a6900fffb70
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-02-20 11:47:26 +0300

    Исправил тип поля id в OrderResponse

--- src/modules/order/response/order.response.ts
+++ src/modules/order/response/order.response.ts
@@ -5,7 +5,7 @@ import UserResponse from '../../user/response/user.response.js';
 export default class OrderResponse {
   @Transform(({obj}) => obj._id.toString())
   @Expose({name: '_id'})
-  public id!: number;
+  public id!: string;
 
   @Expose()
   public createdAt!: string;
